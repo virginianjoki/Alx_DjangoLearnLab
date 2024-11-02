69318960a44cb375c66c1b35919c937e82e7efca
virginianjoki <njokivirginia0619@gmail.com>
2024-11-02 15:27:06 +0300

    book

---
+++ Introduction_to_Django/LibraryProject/bookshelf/create.md
@@ -0,0 +1,5 @@
+#create 
+book = Book(title="1984", author ="George Orwell", publication_date= 1949)
+book.save()
+#expected outcome
+#
--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    authors = models.CharField(max_length=100)
+    publication_date = models.IntegerField()
