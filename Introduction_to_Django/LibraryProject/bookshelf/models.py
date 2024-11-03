f97b45a67cb79e38acb4b492a78b8bbbfe7dd758
virginianjoki <njokivirginia0619@gmail.com>
2024-11-03 21:42:46 +0300

    bookadmin

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -5,3 +5,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     authors = models.CharField(max_length=100)
     publication_date = models.IntegerField()
+
+    def __str__(self):
+        return self.title
