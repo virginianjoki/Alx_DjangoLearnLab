777e82a19c7a45d259b6a7024282ad3ed022f077
virginianjoki <njokivirginia0619@gmail.com>
2024-12-05 20:47:26 +0300

    advanced-api

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -15,7 +15,7 @@ class BookSerializer(serializers.ModelSerializer):
         return value
 
 class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many = True, read_only = True)
+    books = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
