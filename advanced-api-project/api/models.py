524c624c081b504a2a055c8bc243e6b4db50aeff
virginianjoki <njokivirginia0619@gmail.com>
2024-12-05 20:41:38 +0300

    advanced-api

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -14,3 +14,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
+
+    
+
