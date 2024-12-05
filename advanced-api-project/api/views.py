20e59f3f32ade7dbc0adfe873cd8ddfd82055ab0
virginianjoki <njokivirginia0619@gmail.com>
2024-12-05 22:03:25 +0300

    advanced-api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
