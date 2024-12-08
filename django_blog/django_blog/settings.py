9c99f0ac63b36b76fef27765f28dff89be5704f7
virginianjoki <njokivirginia0619@gmail.com>
2024-12-08 21:54:02 +0300

    static

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,6 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_db_user',
+         'PORT': '5432',
+
     }
 }
 
