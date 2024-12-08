5b27616a9b82ec63cbc787082a514059a852f39e
virginianjoki <njokivirginia0619@gmail.com>
2024-12-08 21:48:14 +0300

    static

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User", "title", "content", "author", "published_date" 
+from taggit.managers import TaggableManager
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
+    author =models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class Tag(models.Model):
+    name = models.ManyToManyField(Post, verbose_name='list of post_tags')
+
+    def __str__(self):
+        return self.name
