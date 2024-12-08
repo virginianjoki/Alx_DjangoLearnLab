418c32614c90f698bc988e4478b30f337d36758d
virginianjoki <njokivirginia0619@gmail.com>
2024-12-08 23:16:53 +0300

    templates

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,13 +1,33 @@
-# forms.py
 from django import forms
-from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+from .models import Comment,Post
+from taggit.forms import TagWidget 
+
+
+#custom registration
+class CustomUser(UserCreationForm):
+    email = forms.EmailField(required = True)
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+# user update form to update username and email fields
+class UpdateForm(forms.ModelForm):
+        class Meta:
+            model = User
+            fields = ["username", "email", 'password' ]
 
 class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']  # The fields for creating or editing a post
+     class Meta:
+          model = Post
+          fields = ['title', 'content', 'tags']
+          widgets = {
+            'tags': TagWidget(),
+          }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['title'].widget.attrs.update({'class': 'form-control'})
-        self.fields['content'].widget.attrs.update({'class': 'form-control'})
+class CommentForm(forms.ModelForm):
+     class Meta:
+          model = Comment
+          fields = ['content']
