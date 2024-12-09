21c02e0114224f0888a552ee35b50229ad732d42
virginianjoki <njokivirginia0619@gmail.com>
2024-12-09 13:54:43 +0300

    templates

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name="home"),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/',views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
